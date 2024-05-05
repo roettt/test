02bb3c95df57e731b58b4eb62185e779eed7eaa4
lym <3413956023@qq.com>
2024-05-05 14:47:06 +0800

    fist

---
+++ src/test/java/org/example/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
